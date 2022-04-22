2032dd29794813cb4b518596e85864aac2518ec1
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2022-04-22 22:51:57 +0900

    코코아톡 복습 | 벚꽃 푸딩톡 (CherryBlossom Pudding Talk)
    코코아톡 클론 코딩 공부하면서 스스로도 조금 만들 수 있을 거 같기도 하고 해서 복습 겸 공부로 벚꽃 푸딩톡을 만들어보기로 결심했다!!! 🍮벚꽃 푸딩팅!!!🍮
    푸딩톡 -> 벚꽃 푸딩톡 으로 이름 변경!!
    + 더보기 스크린 만드는 중

---
+++ 벚꽃 푸딩톡으로 복습/profile-edit.js
@@ -0,0 +1,17 @@
+const trashIcon = document.querySelector(".profile__trash-icon-edit");
+
+const photoEdit = document.querySelector(".profile-setting-icon-photo-edit");
+
+function clickPhotoEdit {
+  const ul = document.createElement("ul");
+}
+
+
+function trashIconClick() {
+  alert("삭제하시겠습니까?");
+  alert("삭제되었습니다");
+}
+
+
+trashIcon.addEventListener("click", trashIconClick);
+photoEdit.addEventListener("click", clickPhotoEdit);
