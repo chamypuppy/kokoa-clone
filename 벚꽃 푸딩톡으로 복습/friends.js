1552abea18d0088f3c5988848fcc903a776a064a
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2022-05-09 21:12:39 +0900

    코코아톡 복습 | 벚꽃 푸딩톡 (CherryBlossom Pudding Talk)
    코코아톡 클론 코딩 공부하면서 스스로도 조금 만들 수 있을 거 같기도 하고 해서 복습 겸 공부로 벚꽃 푸딩톡을 만들어보기로 결심했다!!! 🍮벚꽃 푸딩팅!!!🍮
    푸딩톡 -> 벚꽃 푸딩톡 으로 이름 변경!!
    view 채널 만드는 중!

---
+++ 벚꽃 푸딩톡으로 복습/friends.js
@@ -0,0 +1,10 @@
+const friendsArrowUp = document.querySelector(".freinds__list-arrow-up");
+const bookmarkArrowUp = document.querySelector(".bookmark__arrow-up");
+const friendsListArrowUp = document.querySelector(".friends-list__arrow-up");
+
+
+function clickArrowDown(){
+  delete.bookmarkArrowUp
+}
+
+friendsArrowUp.addEventListener("click", clickArrowDown);
